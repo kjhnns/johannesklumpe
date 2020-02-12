d7131fa75d38c3b868c7fae4d01656cf0dc2eeea
Johannes Klumpe <johannes.klumpe@gmail.com>
2020-02-12 18:01:20 +0100

    fix: adds padding on x axis for body

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -12,7 +12,7 @@ const Layout = ({ children }) => (
     <>
       <GlobalStyles />
       <Header />
-      <Box maxWidth="700px" m="0 auto" py={4}>
+      <Box maxWidth="700px" m="0 auto" px={3}>
         {children}
       </Box>
       <Footer />
